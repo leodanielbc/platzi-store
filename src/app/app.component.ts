67d2822ed443014dba7a246b0a7b06d6d1a3b700
Leonardo Daniel Benitez <benitez.leonardo06@gmail.com>
2019-10-11 20:50:19 -0500

    Uso de ngFor para recorrer objetos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzi-store';
-
   items=['Leo','Daniel','Maria','Cris'];
+
+  products: Product[] = [
+    {
+      id:'1',
+      image:'assets/images/coladaMoradaBig2.jpg',
+      title:'Colada Morada',
+      price:2.50,
+      description:'Vaso grande'
+    },
+    {
+      id:'2',
+      image:'assets/images/coladaMoradaSmall.jpg',
+      title:'Colada Morada',
+      price:1.00,
+      description:'Vaso pequeño'
+    },
+    {
+      id:'3',
+      image:'assets/images/empanadas.jpg',
+      title:'Empenadas',
+      price:0.25,
+      description:'Empanadas de queso'
+    },
+    {
+      id:'4',
+      image:'assets/images/emborrajados.jpg',
+      title:'Emborrajados',
+      price:0.25,
+      description:'Emborrajados: Crema de leche con platano'
+    }
+  ];
 
   addItem(){
     this.items.push('Nuevo item');

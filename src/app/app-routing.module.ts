ccd2fe6aadba36382031c2de3368a9ca3009336d
Leonardo Daniel Benitez <benitez.leonardo06@gmail.com>
2019-10-15 12:46:25 -0500

    Preparar el proyecto para Lazy Loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { ProductComponent } from './components/product.component';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';

2a9e75fe473cbfb08527984eecada91da84af11f
Leonardo Daniel Benitez <benitez.leonardo06@gmail.com>
2019-10-14 23:03:57 -0500

    Elaboración de la página de detalle de producto(navegacion al product-detail)

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-products',
@@ -6,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
+
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/coladaMoradaBig2.jpg',
+      title: 'Colada Morada',
+      price: 2.50,
+      description: 'Vaso grande'
+    },
+    {
+      id: '2',
+      image: 'assets/images/coladaMoradaSmall.jpg',
+      title: 'Colada Morada',
+      price: 1.00,
+      description: 'Vaso pequeño'
+    },
+    {
+      id: '3',
+      image: 'assets/images/empanadas.jpg',
+      title: 'Empanadas',
+      price: 0.25,
+      description: 'Empanadas de queso'
+    },
+    {
+      id: '4',
+      image: 'assets/images/emborrajados.jpg',
+      title: 'Emborrajados',
+      price: 0.25,
+      description: 'Emborrajados: Crema de leche con platano'
+    }
+  ];
 
   constructor() { }
 

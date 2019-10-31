18d1e7c82764d22637fb1b29f6bd29df351e6df1
Leonardo Daniel Benitez <benitez.leonardo06@gmail.com>
2019-10-31 04:24:20 -0500

    Terminación de la lista de productos y eliminación de un elemento sin llamar al fetchProducts

--- src/app/admin/components/products-list/products-list.component.ts
+++ src/app/admin/components/products-list/products-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from 'src/app/core/services/products/products.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { Product } from 'src/app/product.model';
 
 @Component({
   selector: 'app-products-list',
@@ -10,17 +12,32 @@ export class ProductsListComponent implements OnInit {
 
   products = [];
   displayedColumns: string[] = ['id', 'title', 'price', 'actions'];
+  tableDS: MatTableDataSource<Product>;
 
   constructor(
     private productsService: ProductsService
   ) { }
 
   ngOnInit() {
+    this.fetchProducts();
   }
 
-  fetchProducts(){
-    this.productsService.getAllProducts().subscribe(products=>{
+  fetchProducts() {
+    this.productsService.getAllProducts().subscribe((products) => {
+      this.tableDS = new MatTableDataSource(products);
       this.products = products;
+    });
+  }
+  deleteProduct(id: string) {
+    this.productsService.deleteProduct(id).subscribe(rta => {
+      console.log(rta);
+      this.products.forEach((elemento, indice) => {
+        if(elemento.id === id){
+          this.products.splice(indice, 1);
+        }
+      });
+      this.tableDS = new MatTableDataSource(this.products);
+      console.log(this.products);
     });
   }
 }

1fab5e5bc1f6b7eb5d20e25590165c45e98ec789
Leonardo Daniel Benitez <benitez.leonardo06@gmail.com>
2019-11-02 01:25:44 -0500

    Editar un producto a traves de un formulario

---
+++ src/app/admin/components/product-edit/product-edit.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ProductsService } from 'src/app/core/services/products/products.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { MyValidators } from 'src/app/utils/validators';
+
+@Component({
+  selector: 'app-product-edit',
+  templateUrl: './product-edit.component.html',
+  styleUrls: ['./product-edit.component.scss']
+})
+export class ProductEditComponent implements OnInit {
+
+  form: FormGroup;
+  id: string;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private productsService: ProductsService,
+    private router: Router,
+    private activeRoute: ActivatedRoute
+  ) {
+    this.buildForm();
+  }
+
+  ngOnInit() {
+    this.activeRoute.params.subscribe((params: Params) => {
+      this.id = params.id;
+      this.productsService.getProduct(this.id).subscribe((product) => {
+        this.form.patchValue(product);
+      });
+    });
+  }
+
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      title: ['', [Validators.required]],
+      price: ['', [Validators.required, MyValidators.isPriceValid]],
+      image: [''],
+      description: ['', [Validators.required]]
+    });
+  }
+
+  saveProduct(event: Event){
+    event.preventDefault(); // este evita recargarla pagina del formulario y ejecuta nuestra operacion con normalidad
+    if(this.form.valid){
+      const product = this.form.value;
+      this.productsService.updateProduct(this.id, product).subscribe(newProduct => {
+        console.log(newProduct);
+        this.router.navigate(['./admin/products']);
+      });
+    }
+  }
+
+  get priceField(){
+    return this.form.get('price');
+  }
+}

c25068c14fa13346a4917b0b3a99f81901fc5c5e
Leonardo Daniel Benitez <benitez.leonardo06@gmail.com>
2019-11-06 00:31:40 -0500

    Mejorando el Guard del Admin, para que tenga un redireccionamiento

--- src/app/admin.guard.ts
+++ src/app/admin.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 
@@ -11,14 +11,19 @@ import { AuthService } from './core/services/auth/auth.service';
 export class AdminGuard implements CanActivate {
 
   constructor(
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
   ){}
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.authService.hasUser().pipe(
-      tap(user => console.log(user)),//esta linea de codigo sirve para debugear la parte insterna del metodo
-      map(user => user === null ? false : true)
+      map(user => user === null ? false : true),
+      tap(hasUser => {
+        if (!hasUser) { //si no hay un usuario
+          this.router.navigate(['/auth/login']);
+        }
+      }) // el tap tambien permite ejecutar una accion
     );
   }
 }

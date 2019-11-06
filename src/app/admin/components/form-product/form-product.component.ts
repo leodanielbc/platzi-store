2352db21d01253b3f36c366ded4ae262996d290f
Leonardo Daniel Benitez <benitez.leonardo06@gmail.com>
2019-11-06 01:15:03 -0500

    Subiendo una imagen a Firebase Storage en el form de crear producto (form-product)

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -3,6 +3,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ProductsService } from 'src/app/core/services/products/products.service';
 import { Router } from '@angular/router';
 import { MyValidators } from 'src/app/utils/validators';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { finalize } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-form-product',
@@ -12,11 +15,13 @@ import { MyValidators } from 'src/app/utils/validators';
 export class FormProductComponent implements OnInit {
 
   form: FormGroup;
+  image$: Observable<any>;
 
   constructor(
     private formBuilder: FormBuilder,
     private productsService: ProductsService,
-    private router: Router
+    private router: Router,
+    private storage: AngularFireStorage
   ) {
     this.buildForm();
   }
@@ -29,7 +34,7 @@ export class FormProductComponent implements OnInit {
       id: ['', [Validators.required]],
       title: ['', [Validators.required]],
       price: ['', [Validators.required, MyValidators.isPriceValid]],
-      image: [''],
+      image: ['', [Validators.required]],
       description: ['', [Validators.required]]
     });
   }
@@ -47,6 +52,25 @@ export class FormProductComponent implements OnInit {
 
   get priceField(){
     return this.form.get('price');
+  }
+
+  uploadFile(event){
+    const file = event.target.files[0];
+    const name = file.name;
+    const fileRef = this.storage.ref(name);
+    const task = this.storage.upload(name, file);
+
+    task.snapshotChanges()
+      .pipe(
+        finalize(() => {
+          this.image$ = fileRef.getDownloadURL();
+          this.image$.subscribe((url) => {
+            console.log(url);
+            this.form.get('image').setValue(url);
+          });
+        })
+      )
+      .subscribe();
   }
 
 }
